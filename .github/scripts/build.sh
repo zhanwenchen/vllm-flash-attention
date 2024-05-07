961cfbd277c6fe09d50f25dfa37776e65558ce07
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2024-05-07 00:57:33 +0000

    Github action for build

---
+++ .github/scripts/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+python_executable=python$1
+cuda_home=/usr/local/cuda-$2
+
+# Update paths
+PATH=${cuda_home}/bin:$PATH
+LD_LIBRARY_PATH=${cuda_home}/lib64:$LD_LIBRARY_PATH
+
+# Install requirements
+$python_executable -m pip install wheel packaging
+
+# Limit the number of parallel jobs to avoid OOM
+export MAX_JOBS=2
+# Make sure release wheels are built for the following architectures
+export TORCH_CUDA_ARCH_LIST="8.0 8.6 8.9 9.0+PTX"
+# Build
+$python_executable setup.py bdist_wheel --dist-dir=dist
